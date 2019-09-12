1c166910cb79065d072e5f3535aa0c58e0d0d2a1
kecyGaurab <kecy.gaurab@gmail.com>
2019-09-12 12:11:21 +0300

    add collection preview

--- Projects/ecommerce-crown/src/App.js
+++ Projects/ecommerce-crown/src/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import { Switch, Route  } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
-const Hatpage =()=>(
-  <div>
-    <h1>Hat page</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div >
       <Switch>
       <Route exact path='/' component ={Homepage} />
-      <Route exact path='/hats' component ={Hatpage} />
+      <Route exact path='/shop' component ={ShopPage} />
       </Switch>
       
     </div>
