839e6e094ef91950387de06ba948cae5d4cb805f
kecyGaurab <kecy.gaurab@gmail.com>
2019-09-23 19:43:25 +0300

    configure redux and set up userReducer

--- Projects/ecommerce-crown/src/pages/sign-in-and-sign-up/sign-in/sign-in.jsx
+++ Projects/ecommerce-crown/src/pages/sign-in-and-sign-up/sign-in/sign-in.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+ import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../../../components/form-input/form-input.component";
 import CustomButton from "../../../components/custom-button/custom-button.component";
