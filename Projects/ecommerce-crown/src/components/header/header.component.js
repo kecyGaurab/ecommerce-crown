0f5d3d1c736825a0d354ce9dbe07e8deef3278cc
kecyGaurab <kecy.gaurab@gmail.com>
2019-10-02 12:58:51 +0300

    fixed syling issues

--- Projects/ecommerce-crown/src/components/header/header.component.js
+++ Projects/ecommerce-crown/src/components/header/header.component.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils'
 import "./header.styles.scss";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/mountain.svg";
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
